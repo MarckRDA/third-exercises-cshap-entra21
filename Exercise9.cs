d6f147bff6801030514ccf55544d0fb7ec08fe4e
MarckRDA <marcos.edo132@gmail.com>
2020-08-21 18:00:56 -0300

    Exercise9 uploaded

--- Exercise9.cs
+++ Exercise9.cs
@@ -7,14 +7,15 @@ namespace third_exercises_csharp_entra21
         public static void SumStrings()
         {
             string[] vect = new string[10];
-            var biuldString = "";
+            var biultString = "";
             for (int i = 0; i < vect.Length; i++)
             {
                 Console.WriteLine("Digit a word: ");
                 vect[i] = System.Console.ReadLine();
-                biuldString = (i % 2 == 0) ? biuldString += vect[i] : biuldString;
+                biultString = (i % 2 == 0) ? biultString += vect[i] : biultString;
             }
-            Console.WriteLine(biuldString);
+            Console.WriteLine(biultString);
+            
         }
     }
 }
